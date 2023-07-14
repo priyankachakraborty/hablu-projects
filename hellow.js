8d3df634b75124883913191778d14dc6d3e19443
priyankachakraborty <cpriyankabd@gmail.com>
2023-07-15 02:40:40 +0600

    first commit

---
+++ hellow.js
@@ -0,0 +1,46 @@
+var Number = 200;
+console.log(typeof Number);
+
+var StrType =   'Ami Banglai Gan Gai';
+console.log(StrType);
+console.log(typeof StrType);
+
+var BoolType = true;
+console.log(BoolType);
+console.log(typeof BoolType);
+
+var Rocona ="savdjgasjfsjkfzjkcba jacja gsiczjk mxnm nmjkchv hsfsytfhcj bzbn "
+console.log(Rocona.toUpperCase());
+
+var Rocona1 ='FKSODIFIUEFIJSJFOUIODJFKLJ FHIDHFJDLJFLDKMLKFLDK;F IJISOJFOIJODJFJKDFLJ'
+console.log(Rocona1.toLowerCase());
+var MyFriend ="Sumon Shuvo Lipi Akhi Subrina Shimul";
+console.log(MyFriend.split('Shuvo'))
+console.log(MyFriend.indexOf('Shimul'))
+
+var Num1 = "5.6574";
+var Num2 = 60;
+var Num3=Num1+Num2;
+console.log(Num3);
+console.log(typeof Num3);
+var Num= parseInt(Num1);
+console.log(Num);
+var total = Num+Num2;
+console.log(typeof total);
+console.log(total);
+console.log(parseFloat(Num1));
+
+var digit = -60;
+var value = Math.abs(digit);
+console.log(value);
+var value1 = Math.round(Num1);
+console.log(value1);
+var value2=Math.ceil(Num1);
+console.log(value2);
+var value3=Math.floor(Num1);
+console.log(value3);
+var lotery=Math.random()*100;
+console.log(lotery);
+
+
+
